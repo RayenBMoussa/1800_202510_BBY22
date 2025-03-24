c8939bb66c5cada9063853f59041e299f8374a17
RayenBMoussa <rayenbenmousa@gmail.com>
2025-03-23 22:17:46 -0700

    trying to store users in database, will fix it in a while

--- scripts/firebaseAPI_BBY22.js
+++ scripts/firebaseAPI_BBY22.js
@@ -1,19 +1,18 @@
 //----------------------------------------
 //  Your web app's Firebase configuration
 //----------------------------------------
-var firebaseConfig = {
-    apiKey: "AIzaSyDWbmtHH6121gYgOqinbsz7_1IP-0xNp5Y",
-    authDomain: "intimo-fb628.firebaseapp.com",
-    projectId: "intimo-fb628",
-    storageBucket: "intimo-fb628.firebasestorage.app",
-    messagingSenderId: "196750217072",
-    appId: "1:196750217072:web:fa8489a2b001e8891509b5",
-    measurementId: "G-PK0V0ZV3WC"
-};
+// var firebaseConfig = {
+//     apiKey: "AIzaSyDWbmtHH6121gYgOqinbsz7_1IP-0xNp5Y",
+//     authDomain: "intimo-fb628.firebaseapp.com",
+//     projectId: "intimo-fb628",
+//     storageBucket: "intimo-fb628.firebasestorage.app",
+//     messagingSenderId: "196750217072",
+//     appId: "1:196750217072:web:fa8489a2b001e8891509b5",
+//     measurementId: "G-PK0V0ZV3WC"
+// };
 
-//--------------------------------------------
-// initialize the Firebase app
-// initialize Firestore database if using it
-//--------------------------------------------
-const app = firebase.initializeApp(firebaseConfig);
-const db = firebase.firestore();
+// //--------------------------------------------
+// // initialize the Firebase app
+// // initialize Firestore database if using it
+// //--------------------------------------------
+// const app = firebase.initializeApp(firebaseConfig);
--- scripts/signUp.js
+++ scripts/signUp.js
@@ -3,6 +3,7 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/11.4.0/firebas
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 import { getAuth, createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-auth.js";
+import { getFirestore, doc, setDoc } from "https://www.gstatic.com/firebasejs/11.4.0/firebase-firestore.js";
 // Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: "AIzaSyBajFZOzlOmtXWlmYlioZ2eoWZhU4IcRqE",
@@ -15,9 +16,62 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+// Initialize the FirebaseUI Widget using Firebase.
+var ui = new firebaseui.auth.AuthUI(firebase.auth());
 
-const submit = document.getElementById("submit");
+var uiConfig = {
+    callbacks: {
+      signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+        //setting up data base with static value until later we make dynamic changes.
+        var user = authResult.user;                            // get the user object from the Firebase authentication database
+        if (authResult.additionalUserInfo.isNewUser) {         //if new user
+            db.collection("Users").doc(user.uid).set({         //write to firestore. We are using the UID for the ID in users collection
+                   name: user.displayName,                    //"users" collection
+                   email: user.email,                         //with authenticated user's ID (user.uid)
+                   prefrences: "sports",                      //preferences     
+                   code: 12341,
+                   myNetworks: [1120032,1232032,2340324],
+                   geoLocation:[23123123,1331312],
+                   status:true                         
+            }).then(function () {
+                   console.log("New user added to firestore");
+                   window.location.assign("main.html");       //re-direct to main.html after signup
+            }).catch(function (error) {
+                   console.log("Error adding new user: " + error);
+            });
+        } else {
+            return true;
+        }
+            return false;
+        },
+      uiShown: function() {
+        // The widget is rendered.
+        // Hide the loader.
+        document.getElementById('loader').style.display = 'none';
+      }
+    },
+    // Will use popup for IDP Providers sign-in flow instead of the default, redirect.
+    signInFlow: 'popup',
+    signInSuccessUrl: "index.html",
+    signInOptions: [
+      // Leave the lines as is for the providers you want to offer your users.
+      // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      // firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      // firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
+      // firebase.auth.PhoneAuthProvider.PROVIDER_ID
+    ],
+    // Terms of service url.
+    tosUrl: '<your-tos-url>',
+    // Privacy policy url.
+    privacyPolicyUrl: '<your-privacy-policy-url>'
+  };
 
+  ui.start('#firebaseui-auth-container', uiConfig);
+  
 submit.addEventListener("click", function (event) {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
